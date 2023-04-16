3d5ef51bb2316e7c88043797bf82313cc74d3825
KMinHong <acute7939@gmail.com>
2023-04-16 20:06:17 +0900

    fix:[김민홍] 리뷰등록 기능 및 에러 수정[HEALTH-FOOD-PROCESS-20]

--- src/main/java/com/example/sbsj_process/product/review/service/ReviewService.java
+++ src/main/java/com/example/sbsj_process/product/review/service/ReviewService.java
@@ -2,6 +2,7 @@ package com.example.sbsj_process.product.review.service;
 
 import com.example.sbsj_process.product.review.entity.ProductReview;
 import com.example.sbsj_process.product.review.service.request.ReviewRegisterRequest;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -14,5 +15,8 @@ public interface ReviewService {
 
     List<ProductReview> list(Long productId);
 
-    List<Map<String, Object>> StarRateAverage(Long productId);
+    List<Map<String, Object>> starRateAverage(Long productId);
+
+    void reviewDelete(Long reviewId);
+
     }
